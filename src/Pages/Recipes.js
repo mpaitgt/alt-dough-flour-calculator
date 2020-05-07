344512baa52d9faafa115931e35a063c94ba63d9
Matt Pignatore <mattpigs@gmail.com>
2020-05-06 21:26:25 -0400

    added fetch the edamam api

--- src/Pages/Recipes.js
+++ src/Pages/Recipes.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {keys} from '../hidden-keys';
 
 function Recipes() {
+  const [hits, setHits] = useState([]);
+  const { edamam_api_key, edamam_app_id } = keys;
+  const url = `https://api.edamam.com/search?q=cookies&app_id=${edamam_app_id}&app_key=${edamam_api_key}`;
+
+  fetch(url)
+    .then(res => res.json())
+    .then(data => setHits([...data.hits]));
+
   return (
-    <h1>Recipes</h1>
+    <div>
+      <h1>Recipes</h1>
+      { 
+        hits.length > 0
+        ?
+        hits.map(item => {
+          return (
+            <p>{item.recipe.label}</p>
+          )
+        })
+        :
+        null
+      }
+    </div>
+    
+    
   )
 }
 
