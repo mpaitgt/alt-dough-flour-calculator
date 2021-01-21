47a06e6ef275a9144b1ea23c6461ef63b3237224
Matt Pignatore <mattpigs@gmail.com>
2021-01-21 16:52:27 -0500

    Updates

---
+++ precache-manifest.3faedc41f713dd1e16f5de924314671a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb753bf9a27cf8a8a0867a34ec86592",
+    "url": "/alt-dough-flour-calculator/index.html"
+  },
+  {
+    "revision": "22376cffa44471ea5367",
+    "url": "/alt-dough-flour-calculator/static/css/main.036d45e8.chunk.css"
+  },
+  {
+    "revision": "0cc67ac4b2e2b4af01e5",
+    "url": "/alt-dough-flour-calculator/static/js/2.39070627.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/alt-dough-flour-calculator/static/js/2.39070627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22376cffa44471ea5367",
+    "url": "/alt-dough-flour-calculator/static/js/main.02f9886f.chunk.js"
+  },
+  {
+    "revision": "89238e679b3416fa6ae3",
+    "url": "/alt-dough-flour-calculator/static/js/runtime-main.cb8008ce.js"
+  },
+  {
+    "revision": "31ec11c96f3e736d2ccbb632e4efe4d7",
+    "url": "/alt-dough-flour-calculator/static/media/altdough15.31ec11c9.jpg"
+  }
+]);
