8ac642a69f380baee0c8d3143901e7caa9854bf6
Matt Pignatore <mattpigs@gmail.com>
2021-01-21 16:57:56 -0500

    Updates

---
+++ precache-manifest.013f132fde8b8773f3b381bba4ffcea3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a5bb9b336ca52c0b64eb29badf8da1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "50703e864aa9656cbf96",
+    "url": "./static/css/main.036d45e8.chunk.css"
+  },
+  {
+    "revision": "8d65e8689c01dab892d6",
+    "url": "./static/js/2.4d7b0e88.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.4d7b0e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50703e864aa9656cbf96",
+    "url": "./static/js/main.afc05fcd.chunk.js"
+  },
+  {
+    "revision": "045092d60bf2b509cb77",
+    "url": "./static/js/runtime-main.fef7d999.js"
+  },
+  {
+    "revision": "31ec11c96f3e736d2ccbb632e4efe4d7",
+    "url": "./static/media/altdough15.31ec11c9.jpg"
+  }
+]);
